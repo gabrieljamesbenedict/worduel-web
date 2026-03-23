58039acaddf95eaadbd6f7d1213283d48528639a
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-23 11:08:25 +0800

    feat: add SubtitleHeader

---
+++ src/app/components/SubtitleHeader.tsx
@@ -0,0 +1,7 @@
+const SubtitleHeader = ({children}: any) => {
+  return (
+    <h2 className="text-3xl">{children}</h2>
+  )
+}
+
+export default SubtitleHeader
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
 import TitleHeader from './components/TitleHeader'
+import SubtitleHeader from './components/SubtitleHeader'
 
 const RootPage = () => {
   return (
     <div className='m-8'>
-      <div className='flex flex-col items-center gap-4'>
-        <Image 
+      <div className='flex flex-col items-center gap-16'>
+        <div className='flex flex-col items-center gap-4'>
+          <Image 
           src={'/worduel_logo.png'} 
           alt={'Worduel Logo'} 
           width={200}
           height={200}
-        />
-        <TitleHeader>Worduel</TitleHeader>
-        <p>welcome to worduel</p>
+          />
+          <TitleHeader>Worduel</TitleHeader>
+          <SubtitleHeader>A competitive word-guessing game inspired from Wordle</SubtitleHeader>
+        </div>
+        <div className='flex flex-col items-center gap-16'>
+          <button>Play</button>
+        </div>
       </div>  
     </div>
   )
