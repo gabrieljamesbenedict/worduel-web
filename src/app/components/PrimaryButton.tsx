971950b06893da208b5fab06b71be9bab5ec8586
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-24 23:09:21 +0800

    feat: add PrimaryButton

---
+++ src/app/components/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+const PrimaryButton = ({ children, onClick }: any) => {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        bg-black
+        text-white
+        text-2xl
+        px-8 py-4
+        min-w-50
+        rounded-full
+        cursor-pointer
+      "
+      >
+      {children}
+    </button>
+  )
+}
+
+export default PrimaryButton
