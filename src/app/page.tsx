26ed683504391ad32a72fe7a5bc6e172ad886be7
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-24 23:42:04 +0800

    refactor: change div organizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
+
 import Image from 'next/image'
 import TitleHeader from './components/TitleHeader'
 import SubtitleHeader from './components/SubtitleHeader'
+import PrimaryButton from './components/PrimaryButton'
+import HomePageButtons from './components/HomePageButtons'
 
 const RootPage = () => {
+
   return (
-    <div className='m-8'>
-      <div className='flex flex-col items-center gap-16'>
-        <div className='flex flex-col items-center gap-4'>
-          <Image 
+    <div className='m-16'>
+      <div className='flex flex-col items-center gap-12 text-align'>
+
+        <div className="flex flex-col items-center gap-2 text-align">
+          <Image
           src={'/worduel_logo.png'} 
           alt={'Worduel Logo'} 
           width={200}
           height={200}
           />
           <TitleHeader>Worduel</TitleHeader>
-          <SubtitleHeader>A competitive word-guessing game inspired from Wordle</SubtitleHeader>
         </div>
-        <div className='flex flex-col items-center gap-16'>
-          <button>Play</button>
-        </div>
+
+        <SubtitleHeader>A competitive word-guessing game inspired from Wordle</SubtitleHeader>
+          
+        <HomePageButtons/>
+
       </div>  
     </div>
   )
