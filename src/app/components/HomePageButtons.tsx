5241f5da20b9e2747d7b3f47b9412365911c5fc9
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-31 01:14:56 +0800

    feat: add routing

--- src/app/components/HomePageButtons.tsx
+++ src/app/components/HomePageButtons.tsx
@@ -1,23 +1,27 @@
 'use client'
 
 import { useState } from "react";
+import { useRouter } from 'next/navigation'
 import PrimaryButton from "./PrimaryButton"
 import SecondaryButton from "./SecondaryButton";
 
 const HomePageButtons = () => {
 
     const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    const router = useRouter()
 
     function startButtonPressed() {
         alert("Hello World");
     }
 
     function login() {
-
+        //alert("Login");
+        router.push('/login')
     }
 
     function register() {
-
+        //alert("Register");
+        router.push('/register')
     }
 
     return (
@@ -32,19 +36,6 @@ const HomePageButtons = () => {
             )}
         </div>
     )
-
-    // if (loggedIn) return (
-    //     <div className='flex flex-row items-center gap-4'>
-    //         <PrimaryButton onClick={startButtonPressed}>Play</PrimaryButton>
-    //         <SecondaryButton onClick={startButtonPressed}>Play</SecondaryButton>
-    //     </div>
-    // )
-    // else if (!loggedIn) return (
-    //     <div>
-    //         <PrimaryButton onClick={login}>Login</PrimaryButton>
-    //         <SecondaryButton onClick={register}>Register</SecondaryButton>
-    //     </div>
-    // )
 }
 
 export default HomePageButtons
