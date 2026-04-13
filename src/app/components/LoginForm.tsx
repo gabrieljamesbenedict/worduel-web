895c9e878bf36ecfb4996bc70b659fb9259da4b9
Gabriel James Benedict <benok.loslos@gmail.com>
2026-04-13 13:56:44 +0800

    feat: add simple HTML form for login

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -14,7 +14,15 @@ const LoginForm = () => {
     }
 
     return (
-        <></>
+        <div>
+            <form onSubmit={onSubmit} className="flex flex-col ">
+                <label htmlFor="usernameInput" className="font-semibold text-left">Username</label>
+                <input type="text" name="usernameInput" className="border"/>
+                <br />
+                <label htmlFor="passwordInput" className="font-semibold text-left">Password</label>
+                <input type="text" name="passwordInput" className="border"/>
+            </form>
+        </div>
     )
 }
 
