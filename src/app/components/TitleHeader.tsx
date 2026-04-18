37c9b6d825648268841f7d3c53b1aa391c3a8807
Gabriel James Benedict <benok.loslos@gmail.com>
2026-04-18 09:04:27 +0800

    overhaul: remove all pages and components

--- src/app/components/TitleHeader.tsx
+++
@@ -1,9 +0,0 @@
-import { breeSerif } from "../fonts"
-
-const TitleHeader = ({children}: any) => {
-  return (
-    <h1 className={`${breeSerif.className} text-6xl`}>{children}</h1>
-  )
-}
-
-export default TitleHeader
